8c5810bdbf21bcc057053bc4d642f8164721eb23
DenisLevkovets <levkovets_da@mail.ru>
2018-06-26 16:14:47 +0300

    Шаблон

---
+++ src/calc.h
@@ -0,0 +1,16 @@
+//
+// Created by user on 26.06.2018.
+//
+
+#ifndef UNTITLED_CALC_H
+#define UNTITLED_CALC_H
+#define NUMBER '0'
+
+void push(double);
+double pop(void);
+
+int getop(char []);
+int getch(void);
+void ungetch(int);
+
+#endif //UNTITLED_CALC_H
---
+++ src/getch.c
@@ -0,0 +1,18 @@
+//
+// Created by user on 26.06.2018.
+//
+#include <stdio.h>
+#define BUFSIZE 100
+
+static char buf[BUFSIZE];
+static int bufp = 0;
+
+int getch(void){
+    return  (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+    if (bufp >= BUFSIZE) printf("ungetch: too many characters\n");
+    else buf[bufp++] = c;
+}
+
